d22f0e67fbe7e634b91d5859593297a4c08ced0c
CukorZhong <2450808861@qq.com>
2023-11-08 14:24:27 +0800

    将使用教程抽离出来

--- vue-project/src/views/main/UserView.vue
+++ vue-project/src/views/main/UserView.vue
@@ -1,27 +1,7 @@
 <template>
     <div>
         <!-- 使用说明 -->
-        <div style="float: right;">
-            <el-button type="info" text @click="drawer = true">
-                <p style="color: rgb(167, 35, 199);">使用说明</p>
-            </el-button>
-            <el-drawer v-model="drawer" title="查询用户帮助教程" :with-header="false">
-                <h3>使用说明</h3>
-                <br>
-                <h4>ID</h4>
-                <p>ID &lt; 0：表示查询所有用户</p>
-                <p>ID = 0：表示不使用ID字段</p>
-                <p>ID &gt; 0：表示使用ID字段，此时无论其他字段是否填写都只使用ID字段</p>
-                <br>
-                <h4>其他字段</h4>
-                <p>仅在ID为0时才有效，且字段不填写则不使用该字段查询，填写了就表示使用该字段查询</p>
-                <br>
-                <h4>搜索与清空</h4>
-                <p>选择好使用的字段之后，点击右边的搜索按钮即可开始查询，点击清空按钮即可将输入框还原。</p>
-                <br>
-                <router-link to="/see-use-info">查看教程</router-link>
-            </el-drawer>
-        </div>
+        <user-helper style="float: right;"/>
 
         <!-- 主体部分 -->
         <div style="display: flex; justify-content: center; align-items: center; height: 100px;">
@@ -108,6 +88,7 @@ import request from '../../request';
 import router from '../../router';
 import { useUserStore } from '../../stores/stores'
 import { ElMessage, ElMessageBox } from 'element-plus'
+import UserHelper from '../../components/user/UserHelper.vue'
 import UserCard from '../../components/user/UserCard.vue';
 
 // 用户状态
@@ -138,9 +119,6 @@ const genders = ref([
         label: "女"
     }
 ])
-
-// 使用教程
-const drawer = ref(false)
 
 // 查询所有用户
 function searchUser() {
