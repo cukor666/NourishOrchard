f8880439e72dabcfe07c51f4abf9fe99e8034af0
CukorZhong <2450808861@qq.com>
2023-11-11 18:22:26 +0800

    修改滚动条样式

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -8,4 +8,16 @@
   padding: 0;
   box-sizing: border-box;
 }
+
+/* 滚动条 */
+::-webkit-scrollbar {
+  width: 8px;
+  background-color: #ADCAC3;
+}
+
+/* 滚动条上的滑块 */
+::-webkit-scrollbar-thumb {
+  background-color: #4CBBDA;
+  border-radius: 5px;
+}
 </style>
