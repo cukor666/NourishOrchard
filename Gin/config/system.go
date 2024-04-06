61ae648c3428f0d04fd8865f0d516f5cb6931720
Cukor <2450808861@qq.com>
2024-04-06 19:13:45 +0800

    删除旧版本遗留的文件
    旧版本的不再需要了，所以留着也没有用了

--- Gin/config/system.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// SystemConfig 系统配置
-type SystemConfig struct {
-	Host   string `yaml:"host"`
-	Port   int    `yaml:"port"`
-	Secret string `yaml:"secret"`
-	Issuer string `yaml:"issuer"`
-}
--- vue/src/stores/aside.js
+++
@@ -1,9 +0,0 @@
-import { ref } from "vue";
-import { defineStore } from "pinia";
-
-export const useAsideStore = defineStore("aside", () => {
-  const isCollapse = ref(true);
-  // 侧边栏宽度
-  const asideWidth = ref(64);
-  return { isCollapse, asideWidth };
-});
