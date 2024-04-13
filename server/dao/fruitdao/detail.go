a6db67c9d2f10b5fedff3c9dd1e84b44c7b68050
Cukor <2450808861@qq.com>
2024-04-13 23:19:06 +0800

    完成水果详情优化

--- server/dao/fruitdao/detail.go
+++ server/dao/fruitdao/detail.go
@@ -2,25 +2,18 @@ package fruitdao
 
 import (
 	"fmt"
-	"server/common/levellog"
 	"server/dao"
+	"server/dto/fruitdto"
 	"server/models"
-	"server/response"
 )
 
 // Detail 水果详情
-func Detail(id int) (res response.FruitDetailResponse, err error) {
-	var fm models.Fruit
-	var sm models.Supplier
-	db := dao.GetMySQL()
-	err = db.Table(fmt.Sprintf("%s f", fm.TableName())).
+func Detail(id int) (res fruitdto.Detail, err error) {
+	err = dao.GetMySQL().Table(fmt.Sprintf("%s f", (&models.Fruit{}).TableName())).
 		Select("f.id fId", "f.`name` fName", "f.water fWater", "f.sugar fSugar",
 			"f.shelf_life fShelfLife", "f.origin fOrigin", "s.id sId", "s.`name` sName",
 			"s.address sAddress", "s.contact_person sContactPerson", "s.phone sPhone", "s.reputation sReputation").
-		Joins(fmt.Sprintf("JOIN %s s ON f.supplier_id = s.id", sm.TableName())).
+		Joins(fmt.Sprintf("JOIN %s s ON f.supplier_id = s.id", (&models.Supplier{}).TableName())).
 		Where("f.id = ?", id).Take(&res).Error
-	if err != nil {
-		levellog.Dao(fmt.Sprintf("查询失败res = %v", res))
-	}
 	return
 }
--- server/response/code/statucode.go
+++
@@ -1,8 +0,0 @@
-package code
-
-// 全局状态码
-const (
-	NotFoundAccount = 4005 // 找不到对应的账号
-	PasswordFailed  = 4006 // 密码错误
-	SystemError     = 5000 // 系统错误
-)
