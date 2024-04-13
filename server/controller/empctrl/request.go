a49397a3b6444bd89aeaab55698c132859ed72cf
Cukor <2450808861@qq.com>
2024-04-13 23:00:17 +0800

    优化晋升管理员接口
    最外层request包全部被优化完成，request包已被删除

--- server/controller/empctrl/request.go
+++ server/controller/empctrl/request.go
@@ -20,3 +20,11 @@ func (ueq *UpdateEmpReq) ToEmp() models.Employee {
 		Salary:   ueq.Salary,
 	}
 }
+
+type promoteReq struct {
+	Username string `json:"username" form:"username" binding:"required,username"`
+	Password string `json:"password" form:"password" binding:"required,min=3,max=20"`
+	Name     string `json:"name" form:"name" binding:"required,min=2,max=20"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Mark     string `json:"mark" form:"mark" binding:"max=100"`
+}
