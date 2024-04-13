be2f6e51e7d7786971768cfabf4957c2e03f352a
Cukor <2450808861@qq.com>
2024-04-13 20:26:05 +0800

    更新注册路由的写法
    优化了不需要自己在修改wg.Add中的goroutine数量

--- server/router/register.go
+++ server/router/register.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
 	"server/common/levellog"
 	"server/router/routes"
@@ -9,21 +10,44 @@ import (
 
 // 注册路由
 func register(r *gin.Engine) {
+	// 注册接口组
 	api := r.Group("/api")
-	wg := new(sync.WaitGroup)
-	wg.Add(10)
 
-	go routes.BaseRoutes(api, wg)       // 基础路由
-	go routes.ActRoutes(api, wg)        // 账号管理
-	go routes.UserRoutes(api, wg)       // 用户管理
-	go routes.LogoutUserRoutes(api, wg) // 注销用户管理
-	go routes.EmpRoutes(api, wg)        // 员工管理
-	go routes.AdminRoutes(api, wg)      // 管理员管理
-	go routes.FruitRoutes(api, wg)      // 水果管理
-	go routes.SupplierRoutes(api, wg)   // 供应商管理
-	go routes.WarehouseRoutes(api, wg)  // 仓库管理
-	go routes.Inventory(api, wg)        // 库存管理
+	// 注册接口
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel() // 退出时取消注册
 
-	wg.Wait()
+	var wg sync.WaitGroup
+
+	// 注册接口
+	routeList := []func(*gin.RouterGroup, *sync.WaitGroup){
+		routes.BaseRoutes,       // 基础路由
+		routes.ActRoutes,        //账户管理
+		routes.UserRoutes,       //用户管理
+		routes.LogoutUserRoutes, //注销用户管理
+		routes.EmpRoutes,        //员工管理
+		routes.AdminRoutes,      //管理员管理
+		routes.FruitRoutes,      //水果管理
+		routes.SupplierRoutes,   //供应商管理
+		routes.WarehouseRoutes,  //仓库管理
+		routes.Inventory,        //库存管理
+	}
+
+	wg.Add(len(routeList))
+
+	// 启动goroutine
+	for _, route := range routeList {
+		go route(api, &wg)
+	}
+
+	// 等待所有goroutine完成
+	go func() {
+		wg.Wait()
+		cancel()
+	}()
+
+	// 等待退出信号
+	<-ctx.Done()
+
 	levellog.Router("全部接口注册完毕")
 }
