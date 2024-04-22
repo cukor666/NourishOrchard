2e569d702ca4fc09dc6259c2f61fb9024b3a4794
Cukor <2450808861@qq.com>
2024-04-22 16:41:05 +0800

    修改默认分页大小
    放大一点显得内容比较多

--- client/src/hooks/list/usePage.js
+++ client/src/hooks/list/usePage.js
@@ -5,8 +5,8 @@ const { NourishUserTotal } = useSessionKey()
 
 export function usePage() {
     const currentPage = ref(1)
-    const pageSize = ref(3)
-    const pageSizes = ref([3, 6, 10, 15, 20])
+    const pageSize = ref(10)
+    const pageSizes = ref([10, 15, 20])
     const total = ref(Number(sessionStorage.getItem(NourishUserTotal)) || 0)
 
     return {
